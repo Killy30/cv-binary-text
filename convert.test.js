b9f6c17b90e032ab06f3270925607fdd338b6661
Killy30 <killy17za@gmail.com>
2022-07-09 17:18:13 -0400

    testing all characters

--- convert.test.js
+++ convert.test.js
@@ -1,5 +1,17 @@
 const convert = require('./app')
 
-test('convert text', ()=>{
-    expect(convert('H')).toBe('01001000')
+test('convert uppercase', ()=>{
+    expect(convert('A')).toBe('01000001')
+})
+
+test('convert lowercase', ()=>{
+    expect(convert('a')).toBe('01100001')
+})
+
+test('convert special characters', ()=>{
+    expect(convert('?')).toBe('00111111')
+})
+
+test('convert number', ()=>{
+    expect(convert('1')).toBe('00110001')
 })
